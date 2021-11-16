4363dff39e831199601662056970c173e9a86f18
radjifatim <fatimradji1@gmail.com>
2021-11-16 07:08:31 -0800

    0-positive_or_negative.py

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+C=int(input ("enter your number")
+        if c>0:
+        print("your number is: POSITIVE")
+        if c<0:
+        print("your number is : NEGATIVE")
+        if c ==0:
+        print("you number is :ZERO")
+        else:
+        print("sorry")
