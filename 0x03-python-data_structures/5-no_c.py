5965cc51709b728fd42f3ce59b55de931837e48e
radjifatim <fatimradji1@gmail.com>
2021-11-22 05:02:36 -0800

    4-new_in_list.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """remove value c and C from list."""
+    for value in(len(my_list)):
+        if value == "c":
+        if value == "C":
+        print("{:d}".format(my_list[i]))
