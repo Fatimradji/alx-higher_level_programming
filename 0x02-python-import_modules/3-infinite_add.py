e177d953a26cfe1d16df06f7f0cb01a0f0b2f5f3
radjifatim <fatimradji1@gmail.com>
2021-11-22 05:49:15 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,11 +4,11 @@ import sys
 if __name__ != "__main__":
     exit()
 
-i = 0
+f = 0
 result = 0
 for argument in sys.argv:
-    if i != 0:
+    if f != 0:
         result += int(argument)
     else:
-        i += 1
+        f += 1
 print("{:d}".format(result))
