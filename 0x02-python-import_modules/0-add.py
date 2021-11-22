d1df7ee9f92c4395b85fa72ffe6e353212017143
radjifatim <fatimradji1@gmail.com>
2021-11-22 05:15:13 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    f = 1
-    r = 2
-    print("{} + {} = {}".format(f, r, add(f, r)))
+    a= 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
