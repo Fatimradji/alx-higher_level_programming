6a577f99e25fbe7ddf397c9124f3143065bd9546
radjifatim <fatimradji1@gmail.com>
2021-11-22 04:53:27 -0800

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, i, element):
+    """we replace element at a specific"""
+    copy = my_list.copy()
+    if i < 0 or i > len(my_list) - 1:
+        return my_list.copy()
+    else:
+        copy[i] = element
+        return copy
